f35f9de1ec45fc5bdb87b8ce246111db4d68dfc1
Anton <anton.spitsyn@hotmail.com>
2019-07-22 00:40:09 +0300

    new version 0.2

--- bindobj.js
+++ bindobj.js
@@ -1,8 +1,9 @@
 var oracledb = require('oracledb');
-var myDate = new Date(2008, 03, 20, 01, 00, 00, 123);
+//var myDate = new Date(2008, 03, 20, 01, 00, 00, 123);
 //var myID = '180';
+var firstname = 'Clara';
 
 module.exports = {
-   B1: { dir: oracledb.BIND_IN, val: myDate}
+   B1: { dir: oracledb.BIND_IN, val: firstname}
 //   ,B2 { dir: oracledb.BIND_IN, val: myID}
 };
--- select-using-bind.js
+++ select-using-bind.js
@@ -9,13 +9,24 @@ var oracledb = require('oracledb');
 var dbConfig = require('./dbconfig.js');
 var bindobj = require('./bindobj.js');
 
+var cmdArgs = process.argv.slice(2);
 var SQLtext = "";
-var SQLtext0 = "alter session set statistics_level = 'ALL'";
+var SQLtextcursor = "alter session set cursor_sharing = 'EXACT'";
+var SQLtextstat = "alter session set statistics_level = 'ALL'";
 var SQLtext1 = "";
 var SQLtext2 = "SELECT * FROM table(DBMS_XPLAN.DISPLAY_CURSOR(null,null,'ALL ALLSTATS LAST'))";
 
 var slogfile = '/temp/test001.log';
 var pathToFile = 'sqltext.txt';
+
+var sqlid = "";
+var SQLtextmem = "";
+
+// force all CLOBs to be returned as Strings
+oracledb.fetchAsString = [ oracledb.CLOB ];
+
+var sqlid = "";
+var clob = "";
 
 var doconnect = function(cb) {
   oracledb.getConnection(
@@ -34,9 +45,14 @@ function doReadFile(callback) {
     });
   };
 
-  function loadSQLtext() {
-    //console.log(SQLtext);
-    SQLtext1 = SQLtext;
+function loadSQLtext() {    
+    SQLtext1 = SQLtext;    
+  };
+
+var checkargs = () => {
+    return (cmdArgs.length == 0 ) ? doReadFile(loadSQLtext)
+    : (cmdArgs.length == 1 || cmdArgs.length == 2 ) ? (sqlid = cmdArgs[0])    
+    : ">2 parameters passed"
   };
 
 var dorelease = function(conn) {
@@ -59,31 +75,76 @@ function simpleout(rows){
      };    
 };
 
-// Pre-command
-var zero = function (conn, cb) {
+// Pre-command1
+var setallstat = function (conn, cb) {
     conn.execute(
-      SQLtext0,
+      SQLtextstat,
       {},  
       function(err, result) {
         if (err) {
           return cb(err, conn);
         } else {
-          return cb(null, conn);
+          return cb(null,null,conn);
         }
       });
   };
 
-// Object query
-var basic = function (conn, cb) {
+// Pre-command1
+var setcursorsharing = function (arg1,conn, cb) {  
+  if (cmdArgs.length == 2) {    
   conn.execute(
-    SQLtext1,
+    SQLtextcursor,
+    {},  
+    function(err, result) {
+      if (err) {
+        return cb(err, conn);
+      } else {
+        return cb(null,arg1,conn);
+      }
+    }) } else {
+       return cb(null,arg1,conn)
+    }
+};
+
+//Get sql text
+var getselect = function (arg1,conn,cb)  {  
+  SQLtextmem = "select sql_fulltext from v$sql where sql_id='" + sqlid + "'";  
+  if (cmdArgs.length == 1 || cmdArgs.length == 2) {
+  conn.execute(
+    SQLtextmem,
+    function(err,result)  {
+        if (err) {console.error(err.message); return;}
+        if (result.rows.length === 0)
+        console.error("No results");
+        else {
+            clob = result.rows[0][0];            
+      //console.log(clob);
+      return cb(null, clob,conn);    
+        }
+    })
+  } else {
+    return cb(null, null,conn)
+  } 
+}; 
+
+var getsqltext = (argx) => {
+  return (cmdArgs.length == 0) ? SQLtext1
+    : (cmdArgs.length == 1) ? argx
+    : (cmdArgs.length == 2) ? argx
+    : "Nothing"
+};
+
+// Object query
+var basic = function (arg1, conn, cb) {
+  conn.execute(
+    getsqltext(arg1),
     bindobj,
     function(err, result) {
       if (err) {
         return cb(err, conn);
       } else {
         //console.log(result.metaData);
-            console.log("Number of rows: " + result.rows.length);        
+        //console.log("Number of rows: " + result.rows.length);        
            // simpleout(result.rows);   
         return cb(null, conn);
       }
@@ -107,12 +168,14 @@ var extended = function (conn, cb) {
     });
 };
 
-doReadFile(loadSQLtext);
+checkargs();
 
 async.waterfall(
   [
-    doconnect,
-    zero,
+    doconnect,    
+    setallstat,
+    setcursorsharing,    
+    getselect,    
     basic,
     extended
   ],
